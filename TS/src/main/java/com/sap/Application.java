a8c8e22e20a76c4fb087c676b438a279be14545b
lipeih0 <peihua.li@sap.com>
2018-09-02 18:34:54 +0800

    upload a material and save it in local disk

--- TS/src/main/java/com/sap/Application.java
+++ TS/src/main/java/com/sap/Application.java
@@ -1,9 +1,11 @@
 package com.sap;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sap.service.StorageProperties")
 public class Application {
 
     public static void main(String[] args) {
--- TS/src/main/java/com/sap/controller/MultistepController.java
+++ TS/src/main/java/com/sap/controller/MultistepController.java
@@ -26,7 +26,7 @@ public abstract class MultistepController {
 
     abstract String getChain(Model model, int start, int limit);
     abstract String getCourse(Model model, Integer chainId, int start, int limit);
-    abstract String getMaterial(Model model, Integer courseId);
+//    abstract String getMaterial(Model model, Integer courseId);
 
     protected void setPageInfo(Model model, PageInfo pageInfo){
         model.addAttribute("currentPage", pageInfo.getPageNum());
