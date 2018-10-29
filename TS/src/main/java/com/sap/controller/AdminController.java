de7353869a242a0edc1fb79f14c6e909727e2289
lipeihua <peihua.li@sap.com>
2018-10-29 11:10:38 +0800

    upload and download on 10/29

--- TS/src/main/java/com/sap/controller/AdminController.java
+++ TS/src/main/java/com/sap/controller/AdminController.java
@@ -14,12 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 @Controller
@@ -67,12 +64,15 @@ public class AdminController extends MultistepController {
         return "course_detail";
     }
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String uploadFile(HttpServletRequest req, HttpSession session, MultipartHttpServletRequest multiReq) throws IOException {
+    public String uploadFile( HttpSession session, MultipartHttpServletRequest multiReq) throws IOException {
 
 
         MultipartFile file = multiReq.getFile("file");
         String fileName = file.getOriginalFilename();
-        FileOutputStream fos = new FileOutputStream(new File("Documents://"+fileName));
+        String pathWebroot = this.getClass().getResource("/").toString();
+        pathWebroot = pathWebroot + "static/";
+        String path = pathWebroot.substring(5);
+        FileOutputStream fos = new FileOutputStream(new File(path +fileName));
         FileInputStream fs = (FileInputStream) file.getInputStream();
         byte[] buffer = new byte[1024];
         int len = 0;
@@ -85,9 +85,53 @@ public class AdminController extends MultistepController {
         String fileType = fileName.substring(fileName.lastIndexOf("."), fileName.length());
         Material newMaterial = new Material();
         newMaterial .setFileType(fileType);
+        log.info("path:" + path +fileName);
         newMaterial.setMaterialName(fileName);
+        newMaterial.setAttachFilepath(path +fileName);
         newMaterial.setCourseId((Integer) session.getAttribute("courseId"));
         materialService.addMaterial(newMaterial);
-        return "redirect:/course_detail?courseId=" + newMaterial.getCourseId();
+        log.info("courseId=" + newMaterial.getCourseId());
+        return "redirect:/admin/material?courseId=" + newMaterial.getCourseId();
+    }
+    @RequestMapping(value = "/download")
+    public void download( @RequestParam(value="materialId") Integer materialId,HttpServletResponse response){
+        Material material = materialService.selectMaterialById(materialId);
+      String path =  material.getAttachFilepath();
+      log.info("download file:"+path);
+
+        File file = new File(path);
+        if(file.exists()){
+            response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" + path);
+
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null; //文件输入流
+            BufferedInputStream bis = null;
+
+            OutputStream os = null; //输出流
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while(i != -1){
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
     }
 }
--- TS/src/main/java/com/sap/service/MaterialService.java
+++ TS/src/main/java/com/sap/service/MaterialService.java
@@ -1,9 +1,9 @@
 package com.sap.service;
 
 
-import com.sap.domain.Material;
+		import com.sap.domain.Material;
 
-import java.util.List;
+		import java.util.List;
 
 public interface MaterialService {
 	List<Material> selectMaterialByCourse(Integer courseId);
